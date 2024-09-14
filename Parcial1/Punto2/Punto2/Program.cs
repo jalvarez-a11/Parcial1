45545b974d841c9ef635cce9c25ce15cf2062712
jose manuel alvarez <jalvarezalvarez30@gmail.com>
2024-09-13 21:30:46 -0500

    Punto2

---
+++ Parcial1/Punto2/Punto2/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el coeficiente a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el coeficiente b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("La ecuación tiene infinitas soluciones.");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no tiene solución.");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine("La solución de la ecuación es x = " + x);
+        }
+    
+
+
+
+    }
+}
